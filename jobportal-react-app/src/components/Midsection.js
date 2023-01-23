10a9cf45bfc06567ae11c740e4b9c0ed84b47357
Gaja Jothi <gajalakshmi.a@gmail.com>
2023-01-23 12:39:17 +0530

    change to midsection.js

--- jobportal-react-app/src/components/Midsection.js
+++ jobportal-react-app/src/components/Midsection.js
@@ -1,9 +1,45 @@
 import React from "react"
+import './midsection.css'
 
 export default function MidSection(){
 	return(
-        <div>
-            Midsection
+        <div className="midsection">
+           <div className="snapshot_outside_boundary">
+            <h5 className="snapshot">Snapshots</h5>
+            <div className="snapshot_inner_item_boundary">
+                <div className="all_job_section">
+                   <div className="all_job_inner_frame">
+                    <div className="all_job_icon_frame all_job_icon_ecllipse">
+                        <div className=" all_job_icon_vector">
+                           <div></div>
+                        </div>
+                    </div>
+                    <div className="_all_job_text_display">
+                        <div className="all_job_text1">
+                    All Jobs
+                        </div > 
+                        <div className="all_job_text2">
+                        100
+                        </div>
+                    </div>
+                    <div className="all_jobs_right_arrow">
+
+                    </div>
+                    
+                    </div>
+                </div>
+                <div>Eligible</div>
+                <div>Applied Job</div>
+            </div>
+           </div>
+           <div>
+            <h5> My Application</h5>
+                <div>ShortListed</div>
+                <div>Interviewing</div>
+                <div>Rejected</div>
+                <div>Offer Recieved</div>
+           </div>
+           <div></div>
         </div>
 	)
 }
